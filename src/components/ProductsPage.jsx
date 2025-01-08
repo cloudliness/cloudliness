609505dd6a9f457a778db52aa2c25e4d4809dac0
Brian <cvryurearsnow@gmail.com>
2025-01-07 23:30:24 -0800

    Update ProductsPage.jsx

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import { Container, Row, Col, Card, Button, Carousel } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Row, Col, Card, Button, Carousel, Modal } from 'react-bootstrap';
 import products from '../seed';
 import { useCart } from '../contexts/CartContext';
 
 const ProductsPage = () => {
   const { addToCart } = useCart();
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalImages, setModalImages] = useState([]);
 
   const handleAddToCart = (product) => {
     addToCart({
       ...product,
       price: product.price
     });
+  };
+
+  const openModal = (images) => {
+    setModalImages(images);
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
   };
 
   return (
@@ -20,18 +31,20 @@ const ProductsPage = () => {
         {products.map(product => (
           <Col key={product.id}>
             <Card className="h-100 shadow-sm">
-              <Carousel interval={null} indicators>
-                {product.images.map((image, index) => (
-                  <Carousel.Item key={index}>
-                    <img
-                      className="d-block w-100"
-                      src={image}
-                      alt={`${product.name} - Image ${index + 1}`}
-                      style={{ height: '200px', objectFit: 'cover' }}
-                    />
-                  </Carousel.Item>
-                ))}
-              </Carousel>
+              <div onClick={() => openModal(product.images)} style={{ cursor: 'pointer' }}>
+                <Carousel interval={null} indicators>
+                  {product.images.map((image, index) => (
+                    <Carousel.Item key={index}>
+                      <img
+                        className="d-block w-100"
+                        src={image}
+                        alt={`${product.name} - Image ${index + 1}`}
+                        style={{ height: '200px', objectFit: 'cover' }}
+                      />
+                    </Carousel.Item>
+                  ))}
+                </Carousel>
+              </div>
               <Card.Body className="d-flex flex-column">
                 <Card.Title>{product.name}</Card.Title>
                 <Card.Text className="flex-grow-1 mb-2">
@@ -51,6 +64,25 @@ const ProductsPage = () => {
           </Col>
         ))}
       </Row>
+
+      <Modal show={modalVisible} onHide={closeModal} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Product Images</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Carousel interval={null} indicators>
+            {modalImages.map((image, index) => (
+              <Carousel.Item key={index}>
+                <img
+                  className="d-block w-100"
+                  src={image}
+                  alt={`Product Image ${index + 1}`}
+                />
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        </Modal.Body>
+      </Modal>
     </Container>
   );
 };
