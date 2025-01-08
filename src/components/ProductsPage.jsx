50673ccd73b18ff9639a5394e1453b21aafbbce1
Brian <cvryurearsnow@gmail.com>
2025-01-07 23:27:55 -0800

    Update ProductsPage.jsx

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button, Carousel } from 'react-bootstrap';
 import products from '../seed';
 import { useCart } from '../contexts/CartContext';
 
@@ -20,21 +20,26 @@ const ProductsPage = () => {
         {products.map(product => (
           <Col key={product.id}>
             <Card className="h-100 shadow-sm">
-              <Card.Img 
-                variant="top" 
-                src={product.images[0]} 
-                alt={product.name}
-                style={{ height: '200px', objectFit: 'cover' }}
-              />
+              <Carousel interval={null} indicators>
+                {product.images.map((image, index) => (
+                  <Carousel.Item key={index}>
+                    <img
+                      className="d-block w-100"
+                      src={image}
+                      alt={`${product.name} - Image ${index + 1}`}
+                      style={{ height: '200px', objectFit: 'cover' }}
+                    />
+                  </Carousel.Item>
+                ))}
+              </Carousel>
               <Card.Body className="d-flex flex-column">
                 <Card.Title>{product.name}</Card.Title>
-                <Card.Text 
-                  className="flex-grow-1"
-                  dangerouslySetInnerHTML={{ __html: product.description }}
-                />
+                <Card.Text className="flex-grow-1 mb-2">
+                  <div dangerouslySetInnerHTML={{ __html: product.description }} />
+                </Card.Text>
                 <div className="d-flex justify-content-between align-items-center">
                   <h5 className="text-primary">${(product.price / 100).toFixed(2)}</h5>
-                  <Button 
+                  <Button
                     variant="primary"
                     onClick={() => handleAddToCart(product)}
                   >
